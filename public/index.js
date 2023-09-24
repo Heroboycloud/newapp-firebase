d6d077bdac7f93f777ff3b89b80b23bba2af5924
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-09-24 10:05:15 +0100

    modified:   app.js 	modified:   index.html 	modified:   index.js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+//  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,4 +15,4 @@
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const app = firebase.initializeApp(firebaseConfig);
